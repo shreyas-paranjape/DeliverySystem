a99376e5656fcee570569fb84b3f7e7de84a0c93
Shreyas Paranjape <sparanjape89@gmail.com>
2015-06-08 11:34:59 +0530

    web android scaffold

---
+++ client/web/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webApp
+ * @description
+ * # webApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
