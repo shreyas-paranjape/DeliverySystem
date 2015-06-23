5e8ec938478e8c1cd75e07d7c82a6d91407124cb
punit-naik <naik.punit44@gmail.com>
2015-06-23 11:44:20 +0530

    Changes

--- service/delivery-service/src/delivery_service/web/route.clj
+++ service/delivery-service/src/delivery_service/web/route.clj
@@ -8,11 +8,11 @@
 (timbre/refer-timbre)
 
 (defroutes app-routes
-  (ANY "/customer/:id" [id request] (res/customer (conj request {:body (conj (:body request) {:person {:id id}} )})))
+  (ANY "/customer/:person_id" [person_id request] (res/customer (conj request {:body (conj (:body request) {:person {:id person_id}} )})))
   (ANY "/customer/:id/add-items" request (res/create-plan request))
-  (ANY "/customer/:id/order/:id2" [id id2 request] (res/order_specific (conj request {:body (conj (:body request) {:person {:id id}} {:order {:id id2}})})))
-  (ANY "/custmer/:id/order" [id request] (res/order (conj request {:body (conj (:body request) {:person {:id id}})})))
-  (ANY "/product/:id" [id request] (res/product (conj request {:body (conj (:body request) {:product {:site_id id}})})))
-  (ANY "/product/:id/:id2" [id id2 request] (res/product_specific (conj request {:body (conj (:body request) {:product {:site_id id, :id id2}})})))
+  (ANY "/customer/:person_id/order/:order_id" [person_id order_id request] (res/order_specific (conj request {:body (conj (:body request) {:person {:id person_id}} {:order {:id order_id}})})))
+  (ANY "/custmer/:person_id/order" [person_id request] (res/order (conj request {:body (conj (:body request) {:person {:id person_id}})})))
+  (ANY "/product/:site_id" [site_id request] (res/product (conj request {:body (conj (:body request) {:product {:site_id site_id}})})))
+  (ANY "/product/:site_id/:product_id" [site_id product_id request] (res/product_specific (conj request {:body (conj (:body request) {:product {:site_id site_id, :id product_id}})})))
   )
 
