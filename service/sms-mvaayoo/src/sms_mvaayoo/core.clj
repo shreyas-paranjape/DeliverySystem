0988cf0e742dfeab6f6e3750b0003a9277fefc34
punit-naik <naik.punit44@gmail.com>
2015-06-30 15:14:34 +0530

    SMS Project Added

--- service/sms-mvaayoo/src/sms_mvaayoo/core.clj
+++ service/sms-mvaayoo/src/sms_mvaayoo/core.clj
@@ -6,7 +6,7 @@
 		{:form-params 
 			{:user "naik.punit44@gmail.com:sail2boat" ;; User credentials ":" separated ID and password.
 			  :senderID "TEST SMS" ;; SenderID - Needs to be registered and bought ex: GoToGoa
-			  :receipientno "8275298308"
+			  :receipientno "8275298308" ;; Can be comma separated inside the string if you want to send it to multiple phone numbers.
 			  :dcs 0 ;; Data Coding Scheme
 			  :msgtxt "This is Test message" ;; The body of the text message.
 			  :state 4} ;; Mandatory
