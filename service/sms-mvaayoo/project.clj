acd15e72b2d7fd6523191b255a39c778e9d246f2
punit-naik <naik.punit44@gmail.com>
2015-06-30 15:32:57 +0530

    SMS Project Added

--- service/sms-mvaayoo/project.clj
+++ service/sms-mvaayoo/project.clj
@@ -3,4 +3,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-  		  [clj-http "1.1.2"]])
+  		  [clj-http "1.1.2"]]
+  :main sms-mvaayoo.core)
