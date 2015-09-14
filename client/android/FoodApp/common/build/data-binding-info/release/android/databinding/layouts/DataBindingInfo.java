f9f7ecd528f0eec2708c2691cf8832d497afba51
diksha694 <diksha6.94@gmail.com>
2015-09-15 00:31:48 +0530

    deleted build files

--- client/android/FoodApp/common/build/data-binding-info/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="6a851748-1b4a-4617-9ac7-b2628fcae745", modulePackage="common", sdkRoot="/opt/android-sdk",layoutInfoDir="/home/shreyas/workspace/DeliverySystem/client/android/FoodApp/common/build/layout-info/release",exportClassListTo="/home/shreyas/workspace/DeliverySystem/client/android/FoodApp/common/build/layout-info/release/_generated.txt",isLibrary=true,minSdk=14,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
--- client/android/FoodApp/common/build/generated/source/apt/debug/common/BR.java
+++
@@ -1,5 +0,0 @@
-package common;
-
-public class BR {
-    public static int _all = 0;
-}
